a999c8db8bb224c81caa807696211b6dc53603ec
VeronikaMi <nika.mitichashili@gmail.com>
2022-04-05 16:07:28 +0400

    added 2 sections, one responsive , the other not

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors:{
+        'dark-green':'#1e3932',
+        'light-green':'#d4e9e2',
+        'active-green':'#008248',
+        'pastel-green':'#f1f8f6'
+      }
+    },
+  },
+  plugins: [],
+}
